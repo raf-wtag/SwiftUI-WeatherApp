cf304755cb58de1c0f6e572978df89b2dbae7f3c
raf-wtag <fahim.rahman@welldev.io>
2021-05-18 14:57:06 +0600

    Project Created

---
+++ SwiftUI-WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Fahim Rahman on 18/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
